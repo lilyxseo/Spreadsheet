ef8962b2a9236f80c7944bfa229441f7b1abccb3
LilyXSeo <149904626+lilyxseo@users.noreply.github.com>
2026-05-16 14:15:12 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,3 +2,5 @@ export const API_KEY = "AIzaSyAQu7C6B0CGU5d1FroQS6hbleimCKybZRA";
 export const SPREADSHEET_ID = "1KzOcV1V4bcxfsLzhfwXxnIxxZoTRlKh4UYhNizREpCM";
 export const SHEETS = ["Kartu Stock", "RPL", "BULKY", "Barang Masuk", "Barang Keluar"];
 export const FILTERS = ["Semua", ...SHEETS];
+export const SUPABASE_URL = "https://udullprykzdskrxmbbkj.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkdWxscHJ5a3pkc2tyeG1iYmtqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3ODg1NzczNywiZXhwIjoyMDk0NDMzNzM3fQ.fmp3311GtsK5Z6t2ZCevRpt2qZzUyr-W4IjoDh9lBc8";
