a872308d42c04095a1dee147005eec2231ae1577
LilyXSeo <149904626+lilyxseo@users.noreply.github.com>
2026-05-16 15:40:14 +0700

    Revert "Login: default light theme, error visibility, and password toggle alignment"

--- login.js
+++ login.js
@@ -56,7 +56,6 @@ function setPasswordVisibility(isVisible) {
 
 async function init() {
   if (window.lucide) window.lucide.createIcons();
-  clearError();
   const existingSession = await getSession();
   if (existingSession) {
     location.replace('/index.html');
